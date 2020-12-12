20dd37c226ca6ea87511981586b4a307c802da4d
dropscar <dropscar041205@gmail.com>
2020-12-13 02:58:06 +0900

    first test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTestCase(TestCase):
+
+    def setUp(self):
+        self.post = Post.objects.create(name="example", post="example post")
+
+    def test_model_can_create(self):
+        posts = Post.objects.all()
+        self.assertEqual(posts.count(), 1)
+
+    def test_model_can_create_2(self):
+        Post.objects.create(name="example2", post="example post2")
+        posts = Post.objects.all()
+        self.assertEqual(posts.count(), 2)
